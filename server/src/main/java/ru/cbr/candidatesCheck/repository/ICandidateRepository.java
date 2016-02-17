d623c046a0a31f0af005e407c48ab997c4bcfabc
vlusslus <konstantinovr1@gmail.com>
2016-02-17 21:22:55 +0300

    To broke something

--- server/src/main/java/ru/cbr/candidatesCheck/repository/ICandidateRepository.java
+++ server/src/main/java/ru/cbr/candidatesCheck/repository/ICandidateRepository.java
@@ -4,5 +4,5 @@ package ru.cbr.candidatesCheck.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.cbr.candidatesCheck.domen.Candidate;
 
-public interface ICandidateRepository extends JpaRepository<Candidate,Long>{
+public interface ICandidateRepository implements JpaRepository<Candidate,Long>{
 }
